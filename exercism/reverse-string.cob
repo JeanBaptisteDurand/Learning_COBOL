9686a8a7b018b1848976fd773fae7c02885dd085
Beorlor <jbdurand2000@gmail.com>
2025-01-18 06:27:26 +0100

    reverse string

--- exercism/reverse-string.cob
+++ exercism/reverse-string.cob
@@ -3,25 +3,29 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01  WS-STRING   PIC X(64).
-       01  WS-REVERSED PIC X(64).
-       77  WS-LEN      PIC 9(4) BINARY.
+       01  WS-REVERSE PIC X(64).
+       77  WS-LENGTH      PIC 9(4) BINARY.
        77  WS-I        PIC 9(4) BINARY.
        77  WS-J        PIC 9(4) BINARY.
 
        PROCEDURE DIVISION.
        REVERSE-STRING.
 
-           INSPECT WS-STRING
-               TALLYING WS-LEN FOR CHARACTERS.
+           MOVE FUNCTION LENGTH(WS-STRING) TO WS-LENGTH.
+           PERFORM VARYING WS-LENGTH FROM WS-LENGTH BY -1
+               UNTIL WS-LENGTH < 1 OR WS-STRING(WS-LENGTH:1) NOT = ' '
+           END-PERFORM.
 
-           MOVE WS-LEN TO WS-J
+           MOVE WS-LENGTH TO WS-I.
+           MOVE 1 TO WS-J.
+           PERFORM VARYING WS-I FROM WS-LENGTH BY -1
+               UNTIL WS-I < 1
+               MOVE WS-STRING(WS-I:1) TO WS-REVERSE(WS-J:1)
+               ADD 1 TO WS-J
+           END-PERFORM.
 
-           PERFORM VARYING WS-I FROM 1 BY 1
-               UNTIL WS-I > WS-LEN
-               MOVE WS-STRING(WS-J:1) TO WS-REVERSED(WS-I:1)
-               SUBTRACT 1 FROM WS-J
-           END-PERFORM
-
-           MOVE WS-REVERSED TO WS-STRING
-
+           MOVE SPACES TO WS-STRING.
+           MOVE WS-REVERSE TO WS-STRING.
+           MOVE SPACES TO WS-REVERSE.
            .
+
