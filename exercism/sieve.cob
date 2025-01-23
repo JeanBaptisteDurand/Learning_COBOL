9e6f3d07fd41fc4058f2677e7c1b0858e63fb98b
Beorlor <jbdurand2000@gmail.com>
2025-01-23 17:43:05 +0100

    sieve

---
+++ exercism/sieve.cob
@@ -0,0 +1,36 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SIEVE.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-LIMIT PIC 9999.
+       01 WS-RESULT PIC 999 OCCURS 1000 TIMES. 
+       01 WS-COUNT PIC 9999 VALUE 0.
+       01 TEMP PIC 999 OCCURS 1000 TIMES VALUE 1. 
+       01 ITER  PIC 9999.
+       01 ITER2 PIC 9999.
+       01 NUM  PIC 9999.
+       PROCEDURE DIVISION.
+       SIEVE.
+           IF WS-LIMIT IS LESS THAN OR EQUAL TO 1
+               GOBACK
+           END-IF.
+           PERFORM VARYING ITER FROM 1 BY 1 UNTIL ITER = 1000
+              MOVE 0 TO WS-RESULT(ITER)
+           END-PERFORM.
+           PERFORM VARYING NUM FROM 2 BY 1 UNTIL NUM = 1000
+             ADD 1 TO NUM GIVING ITER2
+             PERFORM VARYING ITER FROM ITER2 BY 1 UNTIL ITER = 1000
+              IF FUNCTION MOD(ITER, NUM) = 0
+                 MOVE 0 TO TEMP(ITER)
+              END-IF
+             END-PERFORM
+           END-PERFORM.
+           MOVE 1 TO NUM.
+           PERFORM VARYING ITER FROM 2 BY 1 UNTIL ITER = WS-LIMIT
+              IF TEMP(ITER) = 1
+                 MOVE ITER TO WS-RESULT(NUM)
+                 ADD 1 TO NUM
+                 ADD 1 TO WS-COUNT
+              END-IF
+           END-PERFORM.
